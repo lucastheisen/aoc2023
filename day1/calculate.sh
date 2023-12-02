5781f3c3eed210cfb17f5e097c71fe70891699c7
Lucas Theisen <lucastheisen@pastdev.com>
2023-12-02 16:16:02 -0500

    fix day 1

--- day1/calculate.sh
+++ day1/calculate.sh
@@ -4,8 +4,8 @@ readarray -t lines < <(
   sed 's/[^0-9]//g' < "$(dirname "$(readlink --canonicalize-existing "$0")")/input.txt")
 running=0
 for line in "${lines[@]}"; do
-  total=$(("${line:0:1}"+"${line: -1}"))
-  running=$((running+total))
-  printf '%s: %d + %d = %d (running=%d)\n' "${line}" "${line:0:1}" "${line: -1}" "${total}" "${running}"
+  value=$(("${line:0:1}"*10+"${line: -1}"))
+  running=$((running+value))
+  printf '%s: %d (running=%d)\n' "${line}" "${value}" "${running}"
 done
 printf "Grand total: %d" "${running}"
